3f103895c0d4572ed254f12aac8697baba26c53e
VRXDev2 <thytus.ch@gmail.com>
2023-05-24 14:57:34 +0200

    Create GUI.lua

---
+++ GUI.lua
@@ -0,0 +1,110 @@
+-- Instances:
+
+local YuriX = Instance.new("ScreenGui")
+local Background = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local Frame = Instance.new("Frame")
+local AimlockToggled = Instance.new("TextLabel")
+local Freecam = Instance.new("TextLabel")
+local TotemESP = Instance.new("TextLabel")
+local Aimlock = Instance.new("TextLabel")
+local FreecamToggled = Instance.new("TextLabel")
+local TotemESPToggled = Instance.new("TextLabel")
+
+--Properties:
+
+YuriX.Name = "YuriX"
+YuriX.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+YuriX.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Background.Name = "Background"
+Background.Parent = YuriX
+Background.BackgroundColor3 = Color3.fromRGB(79, 79, 79)
+Background.Position = UDim2.new(0.8, 3, 0, 0)
+Background.Size = UDim2.new(0.2, 0, 0.25, 0)
+
+UICorner.Parent = Background
+
+Frame.Parent = Background
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0.02, 0, 0, 0)
+Frame.Size = UDim2.new(0.99, 0, 0.95, 0)
+
+AimlockToggled.Name = "AimlockToggled"
+AimlockToggled.Parent = Frame
+AimlockToggled.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+AimlockToggled.BackgroundTransparency = 1.000
+AimlockToggled.Position = UDim2.new(0.31, 0, 0, 0)
+AimlockToggled.Size = UDim2.new(0.325, 0, 0.175, 0)
+AimlockToggled.Font = Enum.Font.SourceSansBold
+AimlockToggled.Text = "OFF"
+AimlockToggled.TextColor3 = Color3.fromRGB(226, 0, 0)
+AimlockToggled.TextSize = 16.000
+AimlockToggled.TextWrapped = true
+AimlockToggled.TextXAlignment = Enum.TextXAlignment.Left
+
+Freecam.Name = "Freecam"
+Freecam.Parent = Frame
+Freecam.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Freecam.BackgroundTransparency = 1.000
+Freecam.Position = UDim2.new(0.01, 0, 0.185, 0)
+Freecam.Size = UDim2.new(0.325, 0, 0.175, 0)
+Freecam.Font = Enum.Font.SourceSans
+Freecam.Text = "Freecam - Y"
+Freecam.TextColor3 = Color3.fromRGB(0, 0, 0)
+Freecam.TextSize = 16.000
+Freecam.TextWrapped = true
+Freecam.TextXAlignment = Enum.TextXAlignment.Left
+
+TotemESP.Name = "TotemESP"
+TotemESP.Parent = Frame
+TotemESP.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TotemESP.BackgroundTransparency = 1.000
+TotemESP.Position = UDim2.new(0.01, 0, 0.37, 0)
+TotemESP.Size = UDim2.new(0.325, 0, 0.175, 0)
+TotemESP.Font = Enum.Font.SourceSans
+TotemESP.Text = "TCESP - U"
+TotemESP.TextColor3 = Color3.fromRGB(0, 0, 0)
+TotemESP.TextSize = 16.000
+TotemESP.TextWrapped = true
+TotemESP.TextXAlignment = Enum.TextXAlignment.Left
+
+Aimlock.Name = "Aimlock"
+Aimlock.Parent = Frame
+Aimlock.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Aimlock.BackgroundTransparency = 1.000
+Aimlock.Position = UDim2.new(0.01, 0, 0, 0)
+Aimlock.Size = UDim2.new(0.325, 0, 0.175, 0)
+Aimlock.Font = Enum.Font.SourceSans
+Aimlock.Text = "Aimlock - Q"
+Aimlock.TextColor3 = Color3.fromRGB(0, 0, 0)
+Aimlock.TextSize = 16.000
+Aimlock.TextWrapped = true
+Aimlock.TextXAlignment = Enum.TextXAlignment.Left
+
+FreecamToggled.Name = "FreecamToggled"
+FreecamToggled.Parent = Frame
+FreecamToggled.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+FreecamToggled.BackgroundTransparency = 1.000
+FreecamToggled.Position = UDim2.new(0.31, 0, 0.185, 0)
+FreecamToggled.Size = UDim2.new(0.325, 0, 0.175, 0)
+FreecamToggled.Font = Enum.Font.SourceSansBold
+FreecamToggled.Text = "OFF"
+FreecamToggled.TextColor3 = Color3.fromRGB(226, 0, 0)
+FreecamToggled.TextSize = 16.000
+FreecamToggled.TextWrapped = true
+FreecamToggled.TextXAlignment = Enum.TextXAlignment.Left
+
+TotemESPToggled.Name = "TotemESPToggled"
+TotemESPToggled.Parent = Frame
+TotemESPToggled.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TotemESPToggled.BackgroundTransparency = 1.000
+TotemESPToggled.Position = UDim2.new(0.31, 0, 0.37, 0)
+TotemESPToggled.Size = UDim2.new(0.325, 0, 0.175, 0)
+TotemESPToggled.Font = Enum.Font.SourceSansBold
+TotemESPToggled.Text = "OFF"
+TotemESPToggled.TextColor3 = Color3.fromRGB(226, 0, 0)
+TotemESPToggled.TextSize = 16.000
+TotemESPToggled.TextWrapped = true
+TotemESPToggled.TextXAlignment = Enum.TextXAlignment.Left
