5166875025ba20b9b53d10ee3d18c5606013982b
VRXDev2 <thytus.ch@gmail.com>
2023-06-05 22:42:10 +0200

    Update DreadX.lua

--- MM2/DreadX.lua
+++ MM2/DreadX.lua
@@ -13,23 +13,23 @@ local colors = {
 
 local Window = Library.CreateLib("DreadX | Murder Mystery 2", colors)
 
-local Workspace = game:GetService('Workspace');
-local ReplicatedStorage = game:GetService('ReplicatedStorage');
-local Players = game:GetService('Players');
-local Client = Players.LocalPlayer;
-local RunService = game:GetService('RunService');
-local Workspace = game:GetService("Workspace");
-local Lighting = game:GetService("Lighting");
-local UIS = game:GetService("UserInputService");
-local Teams = game:GetService("Teams");
-local ScriptContext = game:GetService("ScriptContext");
-local CoreGui = game:GetService("CoreGui");
-local Camera = Workspace.CurrentCamera;
-local Mouse = Client:GetMouse();
-local Terrain = Workspace.Terrain;
-local VirtualUser = game:GetService("VirtualUser");
+local Workspace = game:GetService('Workspace')
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+local Players = game:GetService('Players')
+local Client = Players.LocalPlayer
+local RunService = game:GetService('RunService')
+local Workspace = game:GetService("Workspace")
+local Lighting = game:GetService("Lighting")
+local UIS = game:GetService("UserInputService")
+local Teams = game:GetService("Teams")
+local ScriptContext = game:GetService("ScriptContext")
+local CoreGui = game:GetService("CoreGui")
+local Camera = Workspace.CurrentCamera
+local Mouse = Client:GetMouse()
+local Terrain = Workspace.Terrain
+local VirtualUser = game:GetService("VirtualUser")
 
-local Murderer, Sheriff = nil, nil;
+local Murderer, Sheriff = nil, nil
 
 local Character = Client.Character
 local Humanoid = Character:FindFirstChild("Humanoid") or Character:WaitForChild("Humanoid")
@@ -47,7 +47,7 @@ end
 function GetSheriff()
     for i,v in pairs(Players:GetChildren()) do 
         if v.Backpack:FindFirstChild("Gun") or v.Character:FindFirstChild("Gun") and v.Name == "Tool" then
-            return v.Name;
+            return v.Name
         end
         return nil
     end
@@ -78,41 +78,95 @@ end)
 
 
 Autofarm_Section:NewToggle("Toggle", "Toggles autofarm", function(state)
-    getgenv().Autofarm = state;
+    getgenv().Autofarm = state
 
-    if not getgenv().AutofarmTarget then return end;
+    if not getgenv().AutofarmTarget then return end
     if getgenv().AutofarmTarget == "Coin" then
         while getgenv().Autofarm do
-            task.wait();
-            local CoinContainer = Workspace:FindFirstChild("CoinContainer", true);
+            task.wait()
+            local CoinContainer = Workspace:FindFirstChild("CoinContainer", true)
             if CoinContainer and Client.PlayerGui.MainGUI.Game.CashBag.Visible == true then
-                local coin = CoinContainer:FindFirstChild("Coin_Server");
+                local coin = CoinContainer:FindFirstChild("Coin_Server")
                 if coin then
                     repeat
-                        RootPart.CFrame = CFrame.new(coin.Position + Vector3.new(0, 1.5, 0)) * CFrame.Angles(0, 0, math.rad(180));
-                        RunService.Stepped:Wait();
-                        if not getgenv().Autofarm then break end;
-                    until not coin:IsDescendantOf(Workspace) or coin.Name ~= "Coin_Server";
-                    task.wait(1.8);
-                end;
+                        RootPart.CFrame = CFrame.new(coin.Position + Vector3.new(0, 1.5, 0)) * CFrame.Angles(0, 0, math.rad(180))
+                        RunService.Stepped:Wait()
+                        if not getgenv().Autofarm then break end
+                    until not coin:IsDescendantOf(Workspace) or coin.Name ~= "Coin_Server"
+                    task.wait(1.8)
+                end
             else
-                task.wait(1.5);
-            end;
-        end;
+                task.wait(1.5)
+            end
+        end
     else
         while getgenv().Autofarm do
-            wait();
+            wait()
             if Client.PlayerGui.MainGUI.Game.CashBag.Visible == true then
-                RootPart.CFrame = CFrame.new(-121.12338256836, 138.27394104004, 38.946128845215);
-            end;
-        end;
-    end;
+                RootPart.CFrame = CFrame.new(-121.12338256836, 138.27394104004, 38.946128845215)
+            end
+        end
+    end
 
 end)
 
 
 local ESP_Tab = Window:NewTab("ESP")
 local ESP_PlayerESP = ESP_Tab:NewSection("PlayerESP")
+
+local folder = Instance.new("Folder",CoreGui)
+folder.Name = "ESP Holder"
+	
+local function AddBillboard(player)
+    local billboard = Instance.new("BillboardGui",folder)
+    billboard.Name = player.Name
+    billboard.AlwaysOnTop = true
+    billboard.Size = UDim2.fromOffset(200,50)
+    billboard.ExtentsOffset = Vector3.new(0,3,0)
+    billboard.Enabled = false
+
+    local textLabel = Instance.new("TextLabel",billboard)
+    textLabel.TextSize = 20
+    textLabel.Text = player.Name
+    textLabel.Font = Enum.Font.Arial
+    textLabel.BackgroundTransparency = 1
+    textLabel.Size = UDim2.fromScale(1,1)
+
+    if getgenv().AllEsp then
+        billboard.Enabled = true
+    end
+    repeat
+        wait()
+        pcall(function()
+            billboard.Adornee = player.Character.Head
+            if player.Character:FindFirstChild("Knife") or player.Backpack:FindFirstChild("Knife") then
+                textLabel.TextColor3 = Color3.new(1,0,0)
+                if not billboard.Enabled and getgenv().MurderEsp then
+                    billboard.Enabled = true
+                end
+            elseif player.Character:FindFirstChild("Gun") or player.Backpack:FindFirstChild("Gun") then
+                textLabel.TextColor3 = Color3.new(0,0,1)
+                if not billboard.Enabled and getgenv().SheriffEsp then
+                    billboard.Enabled = true
+                end
+            else
+                textLabel.TextColor3 = Color3.new(0,1,0)
+            end
+        end)
+    until not player.Parent
+end
+
+for _,player in pairs(Players:GetPlayers()) do
+    if player ~= Client then
+        coroutine.wrap(AddBillboard)(player)
+    end
+end
+Players.PlayerAdded:Connect(AddBillboard)
+
+Players.PlayerRemoving:Connect(function(player)
+    folder[player.Name]:Destroy()
+end)
+
 
 
 
